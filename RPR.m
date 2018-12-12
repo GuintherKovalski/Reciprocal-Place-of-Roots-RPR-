d74212d05b06e50ec687db093c9d376dc0fd649a
Guinther Kovalski <43643854+GuintherKovalski@users.noreply.github.com>
2018-12-12 11:33:22 -0200

    Update RPR.m

--- RPR.m
+++ RPR.m
@@ -10,7 +10,7 @@ if ~exist('p','var')
      p=1;
 end
 LRR=(1/p)*(tf(num,den))*tf(conv([1 0],fliplr(num)),fliplr(den));
-LRR=1+LRR
+LRR=1+LRR;
 [num1,den1] = tfdata(LRR,'v');
 r=roots(num1);    
 end
